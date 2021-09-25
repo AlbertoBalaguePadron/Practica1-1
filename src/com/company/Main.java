574f3fd97b576f38fc9423cc8c87726b8cbc7c79
ALBERTOBALAGUEPADRON <usr2276@salesianos-lacuesta.net>
2021-09-25 20:30:29 +0100

    Feature: Cambie el código bastante, modifique el seleccionamiento ramdom de las notas y de los nombres conforme a su guardado para poder mostrarlo mejor, tambien Agregé la media y lo muestro por pantalla

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,67 +1,99 @@
 package com.company;
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.Reader;
-import java.io.FileReader;
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Formatter;
 
-public class Main {
-
-    public static void main(String[] args) {
-
-        ArrayList nombres = new ArrayList();
-        ArrayList nombres_notas = new ArrayList();
-        File archivo = null;
-        FileReader reader = null;
-        BufferedReader buffer = null;
-
-        try {
-            archivo = new File("D:/Practica1-1/src/com/company/Listado.csv");
-
-            reader = new FileReader(archivo);
-            buffer = new BufferedReader(reader);
-
-            String linea;
-            while ((linea = buffer.readLine() ) != null) {
-               nombres.add(linea);
-            }
+            public class Main {
 
 
+                public static void main(String[] args) {
 
-            for (int i = 0; i < 4; i++) {
-                int index = (int)(Math.random() * nombres.size());
+                    ArrayList<String> nombres = new ArrayList<String>();
+                    double[] nota = new double[6];
+                    File archivos = null;
+                    FileReader reader = null;
+                    BufferedReader buffer = null;
+                    File archivo = null;
+                    FileWriter writer = null;
+                    PrintWriter pw = null;
 
-                nombres_notas.add(nombres.get(index));
-                    for(int e = 1; e<=4; e++)
-                        nombres_notas.add((float)(Math.random()*10 + 1));
+                    try {
+                        double media = 0.0;
+                        float medialist[] = new float[0];
 
 
-            }
+                        // Archivo de entrada
 
-                System.out.println(nombres_notas);
+                        archivos = new File("D:/Practica1-1/src/com/company/Listado.csv");
+                        reader = new FileReader(archivos);
+                        buffer = new BufferedReader(reader);
 
-/*
-            for(int i = 1; i<=4; i++)
-                System.out.println((float)(Math.random()*10 + 1));
+                        // Archivo de Salida
 
-*/
+                        archivo = new File("D:/Practica1-1/src/com/company/Notas.txt");
+                        writer = new FileWriter(archivo);
+                        pw = new PrintWriter(writer);
+
+                        //Leer nombres
+
+                        String linea;
+                        while ((linea = buffer.readLine() ) != null) {
+                            nombres.add(linea);
+                        }
 
 
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
+
+                        String nombre_alumno = "";
+                        String aArchivo = "";
+                        for (int h = 0; h < 10;h++){
+                            double alumnoSorteado = Math.random() * (nombres.size() + 0) + 1;
+                            nombre_alumno = nombres.get((int) alumnoSorteado);
+
+                            double mediaFinal = 0;
+
+                            for(int y = 1; y < 5; y++){
+                                double numRam = (Math.random() * (10 - 1 ) + 1);
+                                double redondeo = Math.round((numRam * 100.0)/100.0);
+                                nota[y] = redondeo ;
+                                mediaFinal =+ redondeo;
+                            }
+
+                            mediaFinal = (nota[1] + nota[2] + nota[3] + nota[4])/4;
+                            nota[5] = mediaFinal;
+
+
+                            String aluMedia = "El alumno " + nombre_alumno + " ha sacado las siguientes notas: Nota 1: " +
+                                    nota[1] + "| Nota 2: " + nota[2] + " |Nota 3: " + nota[3] + " | Nota 4: " + nota[4] +
+                                    "   | Nota media: " + mediaFinal + "\n";
+                            aArchivo += aluMedia;
+                        }
+
+                        System.out.println(aArchivo);
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    } finally {
+                        try {
+                            if (reader != null) {
+                                reader.close();
+                            }
+                            if (buffer != null) {
+                                buffer.close();
+                            }
+                            if (writer != null) {
+                                writer.close();
+                            }
+                            if (pw != null) {
+                                pw.close();
+                            }
+                        } catch (Exception e2) {
+                            e2.printStackTrace();
+                        }
+                    }
+
                 }
-                if (buffer != null) {
-                    buffer.close();
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
             }
-        }
-
-    }
-}
